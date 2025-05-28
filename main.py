4ca11d41af00ca58c74c32bde031bb0655f351fb
Saswat Sahoo <shashwatdvaliant@gmail.com>
2025-05-28 21:58:28 +0530

    First submission

--- main.py
+++ main.py
@@ -1,22 +1,64 @@
-import pygame as pg
+import pygame
 from constants import *
+from player import *
+from asteroid import *
+from asteroidfield import *
+import sys
 
 def main():
-    pg.init()
+    pygame.init()
     print('Starting Asteroids!')
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
-    screen = pg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    updatable = pygame.sprite.Group() 
+    drawable = pygame.sprite.Group()    
 
+    Player.containers = (updatable, drawable)
+
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    clock = pygame.time.Clock()
+    dt = 0             # time delta
+
+
+
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+    Shot.containers = (shots, updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+
+    asteroidfield = AsteroidField()
     while True:
-        for event in pg.event.get():
-            if event.type == pg.QUIT:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
                 return
             
         screen.fill('black')
-        pg.display.flip()
 
+        for item in drawable:
+            item.draw(screen)
+
+        for item in updatable:
+            item.update(dt)
+
+        # updatable.update(dt)
+
+        for asteroid in asteroids:
+            if player.check_collision(asteroid):
+                print('Game over!')
+                sys.exit()
+            for shot in shots:
+                if asteroid.check_collision(shot):
+                    shot.kill()
+                    asteroid.split()
+
+        pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
+        
     
 
 if __name__ == "__main__":
